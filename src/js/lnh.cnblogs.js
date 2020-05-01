161ae11087e3fa1d63cc3112a459aec70f3a5040
lnh <lnhcode@outlook.com>
2020-05-01 09:36:24 +0800

    Refactor js

--- src/js/lnh.cnblogs.js
+++ src/js/lnh.cnblogs.js
@@ -7,36 +7,45 @@
         postBody: "cnblogs_post_body",
         postCategory: "BlogPostCategory",
         postTagList: "EntryTag",
-        toc: "table_of_content_warp",
+        toc: "lnh-toc",
         digg: "div_digg",
         sideBar: "sideBar",
     };
 
-    function $id(id) {
-        return $("#" + id);
+    function isMobile() {
+        return /.*Mobile.*/.test(navigator.userAgent);
     }
 
-    function showFixedBarCore() {
-        var fixedBar = [];
-        fixedBar.push('<div class="fixed-bar-warp">');
-        fixedBar.push('<div class="fixed-bar">');
+    function isPC() {
+        return !isMobile();
+    }
 
+    function $id(id) {
+        return $('#' + id);
+    }
+
+    function addMobileCss(href) {
+        $id('home').before('<link href="' + href + '" rel="stylesheet">');
+    };
+
+    function appendToolBarToBody() {
+        var toolBar = [];
+        toolBar.push('<div id="lnh-toolbar" class="lnh-toolbar">');
         if ($id(element.postBody).length) {
-            fixedBar.push('<a class="item toc" href="javascript:lnh.toc.toggle();" title="目录"><i class="fa fa-bars"></i></a>');
-            fixedBar.push('<a class="item comments" href="#comment_form_container" title="评论列表"><i class="fa fa-comments"></i></a>');
-            fixedBar.push('<a class="item comment" href="#comment_form" title="写评论"><i class="fa fa-pencil"></i></a>');
+            toolBar.push('<a class="fa fa-bars" href="javascript:lnh.toggleToc();" title="目录"></a>');
+            toolBar.push('<a class="fa fa-comments" href="#comment_form_container" title="评论列表"></a>');
+            toolBar.push('<a class="fa fa-pencil" href="#comment_form" title="写评论"></a>');
         }
 
-        fixedBar.push('<a class="item top" href="javascript:scroll(0,0);" title="返回顶部"><i class="fa fa-arrow-circle-up"></i></a>');
-        fixedBar.push('</div>');
-        fixedBar.push('</div>');
+        toolBar.push('<a class="fa fa-arrow-circle-up" href="javascript:scroll(0,0);" title="返回顶部"></a>');
+        toolBar.push('</div>');
 
-        var fixedBarHtml = fixedBar.join('');
+        var toolBarHtml = toolBar.join('');
 
-        $(element.body).append(fixedBarHtml)
+        $(element.body).append(toolBarHtml)
     }
 
-    function moveDiggCore() {
+    function moveDigg() {
         var $sideBar = $id(element.sideBar);
         if ($sideBar.find(element.digg).length) {
             return true;
@@ -49,155 +58,135 @@
         }
     }
 
-    function copyCategoryAndTagCore() {
+    function copyCategoryAndTag() {
         var categotyHtml = $id(element.postCategory).html();
         var entryTagListHtml = $id(element.postTagList).html();
 
         if (categotyHtml) {
-            var html = "<div class='post-categoty-tags'>";
-            html += "<div class='post-categoty'>";
-            html += categotyHtml;
-            html += "</div>";
-            html += "<div class='post-tags'>";
-            html += entryTagListHtml;
-            html += "</div>";
-            html += "</div>";
+            var html = '<div class="lnh-post-categoty-tags">'
+                + '<div class="post-categoty">' + categotyHtml + '</div>'
+                + '<div class="post-tags">' + entryTagListHtml + '</div>'
+                + '</div>';
             $(html).insertBefore("#topics .postBody");
             return true;
         }
     }
 
-    function getPostBodyHeaderListCore() {
-        var headerList = [];
+    function setAutoId() {
+        $id(element.postBody).find(":header").each(function (index, header) {
+            if (!header.id) {
+                header.id = "auto-id-" + index;
+            }
+        });
+    }
+
+
+    function getTocItemArray() {
+        var tocItemArray = [];
 
         $id(element.postBody).find(":header").each(function (index, header) {
             var $header = $(header);
-            var tagName = header.tagName.toLowerCase();
-            if (tagName === 'h1' ||
-                tagName === 'h2' ||
-                tagName === 'h3') {
-                if (!header.id) {
-                    header.id = "auto_id_" + index;
-                }
-
-                headerList.push({
-                    id: header.id,
-                    text: $header.text(),
-                    tagName: tagName,
-                    offsetTop: parseInt($header.offset().top, 10)
-                });
-            }
+            tocItemArray.push({
+                anchor: $header
+            });
         });
 
-        return headerList;
+        return tocItemArray;
     }
 
-    function buildTableOfContentsHtmlCore() {
-        var headerList = getPostBodyHeaderListCore();
-        var tableOfContentsHtml = [];
+    function buildTocHtml(tocItemArray) {
+        var tocHtml = [];
 
-        if (headerList.length) {
-            tableOfContentsHtml.push('<div id="' + element.toc + '" class="lnh_toc_warp">');
-            tableOfContentsHtml.push('<div class="title">目录</div>');
-            tableOfContentsHtml.push('<div class="toc">');
-            for (var i = 0; i < headerList.length; i++) {
-                var header = headerList[i];
-                var tableOfContentsItemHtml = "<a" +
-                    " href='#" + header.id + "'" +
-                    " id='toc_" + header.id + "'" +
-                    " class='item item-" + header.tagName + "'" +
-                    " title='" + header.text + "'" +
-                    ">" +
-                    header.text +
-                    "</a>";
+        if (tocItemArray.length) {
+            tocHtml.push('<div id="' + element.toc + '" class="lnh-toc">');
+            tocHtml.push('<div class="title">目录</div>');
+            tocHtml.push('<div class="items">');
+            for (var i = 0; i < tocItemArray.length; i++) {
+                var tocItem = tocItemArray[i];
+                var tocItemAnchor = tocItem.anchor;
+                var tocItemAnchorId = tocItemAnchor.attr('id');
+                var tocItemAnchorTagName = tocItemAnchor.get(0).tagName.toLowerCase();
+                var tocItemAnchorText = tocItemAnchor.text();
+                var tocItemHtml = '<a'
+                    + ' href="#' + tocItemAnchorId + '"'
+                    + ' id="toc-' + tocItemAnchorId + '"'
+                    + ' class="item item-' + tocItemAnchorTagName + '"'
+                    + '>'
+                    + tocItemAnchorText
+                    + '</a>';
 
-                tableOfContentsHtml.push(tableOfContentsItemHtml);
+                tocHtml.push(tocItemHtml);
             }
-            tableOfContentsHtml.push('</div>');
-            tableOfContentsHtml.push('</div>');
+            tocHtml.push('</div>');
+            tocHtml.push('</div>');
         }
 
-        return tableOfContentsHtml.join('');
+        return tocHtml.join('');
     }
 
-    function toggleTableOfContentsCore() {
-        var $body = $(element.body);
-        var $toc = $id(element.toc);
-
-        if ($toc.length === 0) {
-            var tocHtml = buildTableOfContentsHtmlCore();
-            if (tocHtml) {
-                $body.append(tocHtml);
-                $toc = $id(element.toc);
-            }
-            if ($toc) {
-                show($toc, $body);
-            }
+    function appendTocToBody() {
+        if ($id(element.toc).length) {
             return;
         }
+        var tocItemArray = getTocItemArray();
+        var tocHtml = buildTocHtml(tocItemArray);
+        $(element.body).append(tocHtml);
+    }
 
-        if ($toc.css("display") === "none") {
-            show($toc, $body);
+    function toggleToc() {
+        $id(element.toc).toggleClass('opened');
+        resetBodyStyle();
+    }
+
+    function resetBodyStyle() {
+        var $body = $(element.body);
+        var $toc = $id(element.toc);
+        if ($toc.hasClass('opened')) {
+            var tocWidth = $toc.outerWidth();
+            $body.css("margin-left", tocWidth + 'px');
         } else {
-            hide($toc, $body);
-        }
-
-        function show($toc, $body) {
-            var width = $toc.width();
-            if (width > 320) {
-                width = 320;
-            }
-            $toc.css("display", "inline-block");
-            $body.css("padding-left", width + "px");
-        }
-
-        function hide($toc, $body) {
-            $toc.css("display", "none");
-            $body.css("padding-left", 0);
+            $body.css("margin-left", '');
         }
     }
 
-    function selectedTableOfContentsItemCore(headerId) {
-        var $items = $id(element.toc).find(".item");
-        var $current = $("#toc_" + headerId);
-        if (!$current.hasClass("current")) {
-            $items.removeClass("current");
-            $current.addClass("current");
+    function selectedTocItem(tocItem) {
+        var $selected = $("#toc-" + tocItem.anchor.attr("id"));
+        if (!$selected.hasClass("selected")) {
+            $id(element.toc).find(".item").removeClass("selected");
+            $selected.addClass("selected");
         }
     }
 
-    function watchWindowScrollCore() {
-        var headerList = getPostBodyHeaderListCore();
+    function watchWindowScrollCore(tocItemArray) {
         var scrollTop = $(window).scrollTop() + 80;
-        for (var i = 0; i < headerList.length; i++) {
-            var current = headerList[i];
-            var next = headerList[i + 1];
-            if (scrollTop > current.offsetTop) {
-                if (next && (scrollTop >= next.offsetTop)) {
+        for (var i = 0; i < tocItemArray.length; i++) {
+            var current = tocItemArray[i];
+            var next = tocItemArray[i + 1];
+            if (scrollTop > current.anchor.offset().top) {
+                if (next && (scrollTop >= next.anchor.offset().top)) {
                     continue;
                 }
-                selectedTableOfContentsItemCore(current.id);
+                selectedTocItem(current);
                 break;
             }
         }
     }
 
     window.lnh = {
-        fixedBar: {
-            show: showFixedBarCore
-        },
-        digg: {
-            move: moveDiggCore
-        },
-        post: {
-            copyCategoryAndTag: copyCategoryAndTagCore,
-        },
-        toc: {
-            buildId: getPostBodyHeaderListCore,
-            toggle: toggleTableOfContentsCore,
-            watchWindowScroll: function () {
-                $(window).scroll(watchWindowScrollCore);
-            }
+        isMobile: isMobile,
+        isPC: isPC,
+        addMobileCss: addMobileCss,
+        appendTocToBody: appendTocToBody,
+        appendToolBarToBody: appendToolBarToBody,
+        toggleToc: toggleToc,
+        moveDigg: moveDigg,
+        copyCategoryAndTag: copyCategoryAndTag,
+        setAutoId: setAutoId,
+        watchWindowScroll: function () {
+            var tocItemArray = getTocItemArray();
+            $(window).scroll(function () {
+                watchWindowScrollCore(tocItemArray);
+            });
         },
         run: function () {
             var functionList = Array.prototype.slice.apply(arguments);
@@ -224,23 +213,17 @@
         }
     };
 
-    window.lnh.isMobile = function () {
-        return navigator.userAgent.match(/.*Mobile.*/);
-    };
-
-    window.lnh.addMobileCss = function () {
-        $id("home").before('<link href="//files.cnblogs.com/files/linianhui/lnh.cnblogs.mobile.css" rel="stylesheet">');
-    };
-
 })(window, document, navigator);
 
+lnh.setAutoId();
+lnh.appendTocToBody();
+lnh.appendToolBarToBody();
+lnh.watchWindowScroll();
+
 if (lnh.isMobile()) {
-    lnh.addMobileCss();
-    lnh.toc.buildId();
-    lnh.run(lnh.post.copyCategoryAndTag);
+    lnh.addMobileCss('//files.cnblogs.com/files/linianhui/lnh.cnblogs.mobile.css');
+    lnh.run(lnh.copyCategoryAndTag);
 } else {
-    lnh.fixedBar.show();
-    lnh.toc.toggle();
-    lnh.toc.watchWindowScroll();
-    lnh.run(lnh.digg.move, lnh.post.copyCategoryAndTag);
+    lnh.toggleToc();
+    lnh.run(lnh.moveDigg, lnh.copyCategoryAndTag);
 }
